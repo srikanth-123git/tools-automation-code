d83996946a50591f0911493aa724a0f351e8362c
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-04-13 12:49:43 +0530

    creating role for prometheus

--- module/main.tf
+++ module/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  #iam _instance_profile = ""
 
   tags = {
     Name    = var.tool_name
@@ -14,4 +15,26 @@ resource "aws_route53_record" "record" {
   zone_id  = var.zone_id
   records  = [aws_instance.instance.public_ip]
   ttl      = 30
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "allow"
+        Sid    = ""
+        Principal = {
+          service = "ec2-amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
 }
