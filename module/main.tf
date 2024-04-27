96288123b3e51e067820049b290c87f3b0b38d6a
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-04-27 16:57:29 +0530

    Adding dummy policy

--- module/main.tf
+++ module/main.tf
@@ -57,7 +57,7 @@ resource "aws_iam_role" "role" {
       Version = "2012-10-17"
       Statement = [
         {
-          Action   = var.policy_resource_list
+          Action   = concat[var.dummy_policy, var.policy_resource_list]
           Effect   = "Allow"
           Resource = "*"
         },
--- module/variable.tf
+++ module/variable.tf
@@ -2,3 +2,6 @@ variable "tool_name" {}
 variable "instance_type" {}
 variable "zone_id" {}
 variable "policy_resource_list" {}
+variable "dummy_policy" {
+  default = "ec2:DescribeInstanceTypes"
+}
