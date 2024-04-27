97a6deef698820d2d0139353b710dfcd2a6fb4b2
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-04-27 16:46:02 +0530

    Adding Vault server

--- variables.tf
+++ variables.tf
@@ -7,8 +7,14 @@ variable "tools" {
 
     grafana = {
       instance_type        = "t3.small"
-      policy_resource_list = ["ec2:DescribeInstances"]
+      policy_resource_list = []
     }
+
+    vault = {
+      instance_type        = "t3.small"
+      policy_resource_list = []
+    }
+
   }
 }
 
