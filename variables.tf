def8d3343d1ac486dc2c71db9356778038576586
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-06-27 23:03:33 +0530

    remove unwanted machines.

--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type        = "t3.small"
-      policy_resource_list = ["ec2:DescribeInstances"]
-    }
-
-    grafana = {
-      instance_type        = "t3.small"
-      policy_resource_list = []
-    }
+#     prometheus = {
+#       instance_type        = "t3.small"
+#       policy_resource_list = ["ec2:DescribeInstances"]
+#     }
+#
+#     grafana = {
+#       instance_type        = "t3.small"
+#       policy_resource_list = []
+#     }
 
     vault = {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
 
-    elk = {
-      instance_type        = "r7i.large"
-      policy_resource_list = []
-    }
+#     elk = {
+#       instance_type        = "r7i.large"
+#       policy_resource_list = []
+#     }
 
     ci-server = {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
 
-    sonarqube = {
-      instance_type        = "t3.large"
-      policy_resource_list = []
-    }
-
-    artifactory = {
-      instance_type        = "t3.large"
-      policy_resource_list = []
-    }
+#     sonarqube = {
+#       instance_type        = "t3.large"
+#       policy_resource_list = []
+#     }
+#
+#     artifactory = {
+#       instance_type        = "t3.large"
+#       policy_resource_list = []
+#     }
 
     jenkins = {
       instance_type        = "t3.large"
