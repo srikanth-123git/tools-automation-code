e764f705c454ae42c02a262d004935578dad8523
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-06-30 23:08:36 +0530

    add webhook plugin

--- roles/jenkins/templates/03-plugins.groovy
+++ roles/jenkins/templates/03-plugins.groovy
@@ -10,10 +10,11 @@ import jenkins.model.*
 import hudson.security.*
 
 final List<String> REQUIRED_PLUGINS = [
-        "workflow-aggregator",
-        "ws-cleanup",
-        "blueocean",
-        "ssh-slaves"
+    "workflow-aggregator",
+    "ws-cleanup",
+    "blueocean",
+    "ssh-slaves"
+    "multibranch-scan-webhook-trigger"
 ]
 
 if (Jenkins.instance.pluginManager.plugins.collect {
